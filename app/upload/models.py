3e331bdec364c4205e18bb83a0d4480110bc2163
d051a <akargavin@gmail.com>
2020-09-22 18:37:05 +0300

    first commit

---
+++ app/upload/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Test(models.Model):
+    test_field = models.IntegerField('Test_field')
+    # test_field2 = models.IntegerField('Test_field')
+
+
+class Test2(models.Model):
+    test_field = models.IntegerField('Test_field')
+    # test_field2 = models.IntegerField('Test_field')
+
+
+class Test3(models.Model):
+    test_field = models.IntegerField('Test_field')
+    # test_field2 = models.IntegerField('Test_field')
